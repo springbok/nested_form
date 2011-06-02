c1aeb8ef843f86ea7fbff0d13a5747c9992b1324
Etienne van Tonder <etiennevt@gmail.com>
2011-06-02 15:40:45 +1000

    Test change in builder

--- lib/nested_form/builder.rb
+++ lib/nested_form/builder.rb
@@ -22,7 +22,9 @@ module NestedForm
       @template.after_nested_form(association) do
         model_object = object.class.reflect_on_association(association).klass.new
         output = %Q[<textarea id="#{association}_fields_blueprint" style="display: none">].html_safe
-        output << fields_for(association, model_object, :child_index => "new_#{association}", &@fields[association])
+	obj = []
+	obj << model_object.send(:new)
+        output << fields_for(obj, model_object, :child_index => "new_#{association}", &@fields[association])
         output.safe_concat('</textarea>')
         output
       end
